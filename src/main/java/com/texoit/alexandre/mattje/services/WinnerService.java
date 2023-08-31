4bac1df0630005373b61dc7c672ca83fdc45ecb6
Alexandre Mattje <alexandremattje@MacBook-Pro-de-Alexandre.local>
2023-08-31 19:53:49 -0300

    created process to prepare min and max winner list

--- src/main/java/com/texoit/alexandre/mattje/services/WinnerService.java
+++ src/main/java/com/texoit/alexandre/mattje/services/WinnerService.java
@@ -1,17 +1,32 @@
 package com.texoit.alexandre.mattje.services;
 
 import com.texoit.alexandre.mattje.dto.ProducerWinner;
+import com.texoit.alexandre.mattje.dto.Winner;
 import com.texoit.alexandre.mattje.dto.WinnerRange;
-import com.texoit.alexandre.mattje.model.Movie;
-import com.texoit.alexandre.mattje.model.Producer;
 import com.texoit.alexandre.mattje.repositories.ProducerRepository;
+import lombok.*;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class WinnerService {
 
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    static class Interval {
+
+        private Integer previous;
+
+        @Setter
+        private Integer following;
+
+        public Integer difference() {
+            return following - previous;
+        }
+    }
     private final ProducerRepository movieService;
 
     public WinnerService(ProducerRepository movieService) {
@@ -19,9 +34,78 @@ public class WinnerService {
     }
 
     public WinnerRange findWinnerRanger() {
+
         List<ProducerWinner> allWinners = movieService.getAllProducerWithWinnerMovies();
-        allWinners.forEach(it -> System.out.println(it.toString()));
-        return WinnerRange.builder().build();
+        Map<String, List<Interval>> producerMapped = allWinners.stream().reduce(
+                new HashMap<String, List<Interval>>(),
+                (map, winner) -> {
+                    List<Interval> intervals = map.get(winner.getProducer());
+                    if (intervals == null) {
+                        intervals = new ArrayList<>();
+                        intervals.add (new Interval(winner.getYear(), 0));
+                    } else {
+                        Interval interval = intervals.get(intervals.size() - 1);
+                        interval.setFollowing(winner.getYear());
+                        intervals.add (new Interval(winner.getYear(), 0));
+                    }
+                    map.put(winner.getProducer(), intervals);
+                    return map;
+                }, (q, w) -> q)
+                    // filter invalid entries need to have at least 2 Interval
+                    .entrySet().stream()
+                    .filter(entry -> entry.getValue().size() > 1)
+                    .collect(Collectors.toMap(Map.Entry::getKey,
+                            entry -> entry.getValue().stream()
+                                    // this filter is to remove all interval without following year
+                                    .filter(it -> it.following > 0).collect(Collectors.toList())));
+
+        return WinnerRange.builder()
+                .min(extractMinWinner(producerMapped))
+                .max(extractMaxWinner(producerMapped))
+                .build();
     }
-    
+
+    private List<Winner> extractMaxWinner(Map<String, List<Interval>> producerMapped) {
+        int max = producerMapped.values().stream().reduce(0, (minFromMap, yearList) ->
+                yearList.stream().reduce(minFromMap, (minFromList, interval) -> {
+                    if (interval.difference() > minFromList) {
+                        return interval.difference();
+                    }
+                    return minFromList;
+                }, (a, b) -> a), (a, b) -> a);
+        return getWinners(producerMapped, max);
+    }
+
+    private List<Winner> extractMinWinner(Map<String, List<Interval>> producerMapped) {
+        int min = producerMapped.values().stream().reduce(99, (minFromMap, yearList) ->
+                yearList.stream().reduce(minFromMap, (minFromList, interval) -> {
+                    if (interval.difference() < minFromList) {
+                        return interval.difference();
+                    }
+                    return minFromList;
+                }, (a, b) -> a), (a, b) -> a);
+        return getWinners(producerMapped, min);
+    }
+
+    private List<Winner> getWinners(Map<String, List<Interval>> producerMapped, int differenceToFind) {
+        return producerMapped.entrySet().stream()
+                .filter(entry -> entry.getValue().stream().anyMatch(it -> it.difference() == differenceToFind))
+                .map(entry -> {
+                    List<Winner> winners = new ArrayList<>();
+                    entry.getValue().forEach(it -> {
+                        winners.add(
+                                Winner.builder()
+                                        .interval(it.difference())
+                                        .producer(entry.getKey())
+                                        .previousWin(it.previous)
+                                        .followingWin(it.following)
+                                        .build());
+                    });
+                    return winners;
+                }).reduce(new ArrayList<>(), (a, b) -> {
+                    a.addAll(b);
+                    return a;
+                });
+    }
+
 }
